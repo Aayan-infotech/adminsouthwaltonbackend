d0ba882507efa5288ad8239f72f80cd978e2dd44
Utkarsh <utkarsh.gupta@aayaninfotech.com>
2024-10-25 13:43:36 +0530

    newbranch

--- routes/driverRoute.js
+++ routes/driverRoute.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const Driver = require('../models/driverModel');
 const {
@@ -42,48 +41,3 @@ router.get('/filtered-bookings', getFilteredBookings);
 router.post('/logout', driverLogout);
 
 module.exports = router;
-=======
-const express = require("express");
-const Driver = require('../models/driverModel');
-const {
-  getFilteredBookings ,
-  updateBookingStatus,
-  createDriver,
-  assignDriverToBooking,
-  getAllDrivers,
-  getDriverBookings,
-  getDriverById,
-  updateDriverById,
-  deleteDriver,
-  getImage,
-  driverLogin,
-  driverLogout
-} = require('../controllers/driverController');
-const { verifyAdmin } = require('../middleware/verifyToken');
-const router = express.Router();
-const upload = require('../middleware/upload');
-
-router.post('/add', upload.single('image'), verifyAdmin, createDriver); 
-router.get('/', verifyAdmin, getAllDrivers);  
-router.get('/:id', verifyAdmin, getDriverById); 
-router.put('/:id', upload.single('image'), verifyAdmin, updateDriverById);      
-router.delete('/:id', verifyAdmin, deleteDriver);  
-router.get('/image/:filename', getImage); 
-
-// Driver Login
-router.post('/login', verifyAdmin, driverLogin);
-
-// Assign driver to a booking
-router.post('/assignDriver', assignDriverToBooking);
-
-// Get bookings assigned to a driver
-router.get('/:driverId/bookings', getDriverBookings);
-
-router.put('/bookings/update-status', updateBookingStatus);
-router.get('/filtered-bookings', getFilteredBookings);
-
-// Driver Logout
-router.post('/logout', driverLogout);
-
-module.exports = router;
->>>>>>> 19c4fd4ae3c973310659d9c4341f3e59c859e45c
