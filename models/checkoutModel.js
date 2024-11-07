5dec0202937f06890d4d94d3d34b2aeaab35b7e9
unknown <mahi.sahai@aayaninfotech.com>
2024-11-07 14:57:35 +0530

    pushnew

--- models/checkoutModel.js
+++ models/checkoutModel.js
@@ -2,76 +2,27 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const BookformSchema = new Schema({
-    // bpickup: {
-    //     type: String,
-    //     required: false,
-    // },
-    // bdrop: {
-    //     type: String,
-    //     required: false,
-    // },
-    // bpickDate: {
-    //     type: Date,
-    //     required: false,
-    // },
-    // bdropDate: {
-    //     type: Date,
-    //     required: false,
-    // },
 
-    bname: {
-        type: String,
-        required: true,
-    },
-    bphone: {
-        type: Number,
-        required: true,
-    },
-    bemail: {
-        type: String,
-        required: true,
-    },
-    bsize: {
-        type: Number,
-        required: true,
-    },
-    baddress: {
-        type: String,
-        required: false,
-    },
-    baddressh: {
-        type: String,
-        required: false,
-    },
-
-    paymentId: {
-        type: Schema.Types.ObjectId,
-        ref: 'Payment',
-        required: false,
-    },
-    reservationId: {
-        type: Schema.Types.ObjectId,
-        ref: 'Reservation',
-        required: false,
-    },
-
-    vehiclesId: {
-        type: String,
-        require: false
-    },
-    driver: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Driver',
-        default: null
-    },
-    status: {
-        type: String,
-        enum: ['PENDING', 'DELIVERED', 'COMPLETED'],
-        default: 'PENDING',
-    },
-
-
-
+bname: {type: String, required: true,  },
+    bphone: { type: Number, required: true, },
+    bemail: {  type: String,  required: true, },
+    bsize: {  type: Number, required: true,  },
+    baddress: { type: String,required: false, },
+    baddressh: { type: String, required: false,},
+    paymentId: { type: Schema.Types.ObjectId, ref: 'Payment',  required: false,  },
+    reservationId: {  type: Schema.Types.ObjectId,  ref: 'Reservation',  required: false,  },
+    vehiclesId: { type: String,  require: false  },
+    driver: { type: mongoose.Schema.Types.ObjectId, ref: 'Driver', default: null },
+    status: {  type: String, enum: ['PENDING', 'DELIVERED', 'COMPLETED'], default: 'PENDING',},
 }, { timestamps: true });
+
+BookformSchema.pre('save', async function (next) {
+    if (this.isModified('status') && this.status === 'COMPLETED') {
+       
+        await this.remove();
+    }
+    next();
+});
+
 
 module.exports = mongoose.model('Bookform', BookformSchema);
