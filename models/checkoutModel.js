b96cc8b95f33a77be4c104bf826ad017e3a70753
unknown <mahi.sahai@aayaninfotech.com>
2024-11-20 11:02:43 +0530

    config

--- models/checkoutModel.js
+++ models/checkoutModel.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -26,3 +27,82 @@ BookformSchema.pre('save', async function (next) {
 
 
 module.exports = mongoose.model('Bookform', BookformSchema);
+=======
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BookformSchema = new Schema({
+    // bpickup: {
+    //     type: String,
+    //     required: false,
+    // },
+    // bdrop: {
+    //     type: String,
+    //     required: false,
+    // },
+    // bpickDate: {
+    //     type: Date,
+    //     required: false,
+    // },
+    // bdropDate: {
+    //     type: Date,
+    //     required: false,
+    // },
+
+    bname: {
+        type: String,
+        required: true,
+    },
+    bphone: {
+        type: Number,
+        required: true,
+    },
+    bemail: {
+        type: String,
+        required: true,
+    },
+    bsize: {
+        type: Number,
+        required: true,
+    },
+    baddress: {
+        type: String,
+        required: false,
+    },
+    baddressh: {
+        type: String,
+        required: false,
+    },
+
+    paymentId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Payment',
+        required: false,
+    },
+    reservationId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Reservation',
+        required: false,
+    },
+
+    vehiclesId: {
+        type: String,
+        require: false
+    },
+    driver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Driver',
+        default: null
+    },
+    status: {
+        type: String,
+        enum: ['PENDING', 'DELIVERED', 'COMPLETED'],
+        default: 'PENDING',
+    },
+
+
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('Bookform', BookformSchema);
+>>>>>>> e832f0f8b607df6b7083eb3ebc979275564f7d56
